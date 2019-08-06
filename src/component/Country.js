26dff6ba81dc9cb4b4bca88b8d3b8a26ae19ae38
Aizhigit <aiji110696@gmail.com>
2019-08-06 11:18:27 +0600

    Search

--- src/component/Country.js
+++ src/component/Country.js
@@ -1,17 +1,50 @@
-import React, { Component } from 'react';
-import CountryContainer from '../container/CountryContainer';
+import React from 'react';
 
+class Country extends React.Component {
+    state = {
+        isShowed: false
+    }
 
-function Country(props) {
-  return (
-    <table border="1">
-    <tr>Country name: {props.data.name}
-    <img alt={props.data.name} src={props.data.flag}></img>
-     Capital: {props.data.capital}
-     <button onClick = {props.deleteCountry}>Delete</button>
-     <button onClick = {props.openModal}>Modal</button>
-     </tr>
-     </table>
-  );
+    toggleShow = () => {
+        this.setState({...this.state, isShowed: !this.state.isShowed})
+    }
+
+    render() {
+
+    const modal = () => {
+    if(this.state.isShowed) {
+
+    return (
+            <div id="popup1" className="overlay">
+              <div className="popup">
+                <h2>Information about: </h2>
+                <a className="close" href="#" onClick={this.toggleShow}>&times;</a>
+                <div className="content">
+                        <div className="code">Code: {this.props.data.alpha2Code}</div>
+                        <div className="flag">Flag: <img src={this.props.data.flag}></img></div>
+                        <div className="country-name">Country name: {this.props.data.name}</div>
+                        <div className="capital">Capital: {this.props.data.capital}</div>
+                        <div className="population">Population: {this.props.data.population}</div>
+                </div>
+              </div>
+            </div> 
+        );
+    }
+}
+
+    return (
+        <li>
+            {modal()}
+            <div className="code">Code: {this.props.data.alpha2Code}</div>
+            <div className="flag">Flag: <img src={this.props.data.flag}></img></div>
+            <div className="country-name">Country name: {this.props.data.name}</div>
+            <div className="capital">Capital: {this.props.data.capital}</div>
+            <div className="population">Population: {this.props.data.population}</div>
+            <button className="button" onClick={this.props.deleteCountry}>Delete</button>
+            <a className="button" onClick={this.toggleShow} href="#popup1">Pop</a>
+        </li>
+    );
+    }
 }
 export default Country;
+
--- src/container/CountryContainer.js
+++ src/container/CountryContainer.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-
+import React, { Component, Fragment } from 'react';
 import Country from '../component/Country';
 import './CountryContainer.css';
 
 class CountryContainer extends Component {
   state = {
-    data: []
+    data: [],
+    searchedCountry: ""
   }
   componentWillMount = () => {
     fetch('https://restcountries.eu/rest/v2/all')
@@ -18,14 +18,29 @@ class CountryContainer extends Component {
       countries.splice(index,1);
       this.setState({data: countries});
     }
+   onchange = aiji => {
+          this.setState({searchedCountry: aiji.target.value});
+
+}
+
   render() {
+    const filCountries = this.state.data.filter ( country => {
+      
+      return country.name.toLowerCase().includes(this.state.searchedCountry.toLowerCase());
+    })
+
     return (
+      <Fragment >
+      <input label="Search" icon="search" 
+      value = {this.state.searchedCountry} onChange={this.onchange}/>
       <ul className="country-list">
-        {this.state.data.map((item, index) => 
-        <Country data = {item}
+        {filCountries.map((item, index) => {
+        return (<Country data = {item}
       deleteCountry =  { () => this.deleteCountry(index)}
-         />)}
+         />);}
+        )}
       </ul>
+      </Fragment>
       );
     }
 }
